67379cde4602ee1a3fa543661199e119fd1fde35
Elias Sharafi <eliassharafi14422@gmail.com>
2021-05-16 06:17:26 +0430

    create app and library for banner slider

---
+++ BannerSliderLibrary/src/main/java/ir/eliassharafi/bannersliderlibrary/BaseBannerAdapter.java
@@ -0,0 +1,76 @@
+package ir.eliassharafi.bannersliderlibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Created by test on 2017/11/22.
+ */
+
+
+public class BaseBannerAdapter extends RecyclerView.Adapter<BaseBannerAdapter.MzViewHolder> {
+
+    private Context context;
+    private List<String> urlList;
+    private BannerLayout.OnBannerItemClickListener onBannerItemClickListener;
+
+    public BaseBannerAdapter(Context context, List<String> urlList) {
+        this.context = context;
+        this.urlList = urlList;
+    }
+
+    public void setOnBannerItemClickListener(BannerLayout.OnBannerItemClickListener onBannerItemClickListener) {
+        this.onBannerItemClickListener = onBannerItemClickListener;
+    }
+
+    @Override
+    public MzViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new MzViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.banner_list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(MzViewHolder holder, final int position) {
+        if (urlList == null || urlList.isEmpty())
+            return;
+        final int P = position % urlList.size();
+        String url = urlList.get(P);
+        ImageView img = (ImageView) holder.imageView;
+        GlideApp.with(context).load(url).placeholder(android.R.drawable.progress_indeterminate_horizontal).error(android.R.drawable.stat_notify_error).into(holder.imageView);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onBannerItemClickListener != null) {
+                    onBannerItemClickListener.onItemClick(P);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if (urlList != null) {
+           return urlList.size();
+        }
+       return 0;
+    }
+
+
+    class MzViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        MzViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.image);
+        }
+    }
+
+}
--- app/src/main/java/ir/eliassharafi/bannerslider/MainActivity.kt
+++ app/src/main/java/ir/eliassharafi/bannerslider/MainActivity.kt
@@ -1,11 +1,35 @@
 package ir.eliassharafi.bannerslider
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ir.eliassharafi.bannersliderlibrary.BannerLayout
+import ir.eliassharafi.bannersliderlibrary.BaseBannerAdapter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val banner: BannerLayout = findViewById<View>(R.id.Banner) as BannerLayout
+
+        val urls: MutableList<String> = ArrayList()
+        urls.add("http://szzljy.com/images/mountain/mountain4.jpg")
+        urls.add("http://szzljy.com/images/mountain/mountain4.jpg")
+        urls.add("http://www.visitgreece.gr/deployedFiles/StaticFiles/Photos/Generic%20Contents/Forests/mountains_2_560.jpg")
+        urls.add("http://szzljy.com/images/mountain/mountain4.jpg")
+        urls.add("http://www.visitgreece.gr/deployedFiles/StaticFiles/Photos/Generic%20Contents/Forests/mountains_2_560.jpg")
+        urls.add("http://szzljy.com/images/mountain/mountain4.jpg")
+
+
+        val webBannerAdapter = BaseBannerAdapter(this, urls)
+        webBannerAdapter.setOnBannerItemClickListener {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        }
+        banner.setAdapter(webBannerAdapter)
+
     }
 }
